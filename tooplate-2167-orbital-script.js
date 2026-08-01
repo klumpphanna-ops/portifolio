a54e1bc717a3ecd03ad2f7c643d29904f84bcda9
klumpphanna-ops <Klumpp.hanna@gmail.com>
2026-08-01 12:24:06 -0300

    alteração js

--- tooplate-2167-orbital-script.js
+++ tooplate-2167-orbital-script.js
@@ -55,11 +55,11 @@
       p.style.setProperty('--tz', r + 'px');
     });
     spacingSteps.forEach(function (b) {
-      b.classList.toggle('is-active', parseInt(b.getAttribute('data-space'), 10) === idx);
+      b.classList.toggle(, parseInt(b.getAttribute('data-space'), 10) === idx);
     });
   }
   spacingSteps.forEach(function (b) {
-    b.addEventListener('click', function () {
+    b.addEventListener('is-active', function () {
       setSpacing(parseInt(b.getAttribute('data-space'), 10));
     });
   });
